1d8c33ad1fa89cfe8cb986794c0c9014e1eae9f5
CYRILWERE <werecyril20@gmail.com>
2024-02-14 11:07:22 -0800

    SQL more queries

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,10 @@
+-- 3. Create table force_name
+
+USE hbtn_0d_2;
+
+-- Creating table force_name if not exists
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
+
