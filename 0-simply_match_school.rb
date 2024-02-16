eada4c45975c3cd0edabb224bc47bfccc83a18fa
CYRILWERE <werecyril20@gmail.com>
2024-02-16 14:24:14 -0800

    regular expressions

--- 0-simply_match_school.rb
+++ 0-simply_match_school.rb
@@ -1,20 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if there is an argument provided
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-  exit 1
-end
-
-# Extract the first argument
-input_string = ARGV[0]
-
-# Define the regular expression pattern
-pattern = /School/
-
-# Match the pattern in the input string
-match = input_string.match(pattern)
-
-# Print the result
-puts match ? match[0] : ""
-
+puts ARGV[0].scan(/School/).join
