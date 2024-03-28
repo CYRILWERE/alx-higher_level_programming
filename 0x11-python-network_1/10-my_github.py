fc35a208974fb2f05ad799b3128efbf7d25f3406
CYRILWERE <werecyril20@gmail.com>
2024-03-28 13:33:59 -0700

    python network 1

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module: 10-my_github
+Uses Basic Authentication with a personal access token to access GitHub API and display user id.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = 'https://api.github.com/user'
+
+    response = requests.get(url, auth=(username, password))
+    if response.status_code == 200:
+        print(response.json()['id'])
+    else:
+        print(None)
+
