c1702c64522ee81d3047c3b97e6c6701257776fe
CYRILWERE <werecyril20@gmail.com>
2024-05-02 13:17:45 -0700

    0x15-javascript-web_jquery

---
+++ alx-higher_level_programming/0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $.ajax({
+        url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+        type: 'GET',
+        success: function (response) {
+            $('#character').text(response.name);
+        }
+    });
+});
+
